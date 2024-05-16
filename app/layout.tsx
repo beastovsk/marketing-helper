dec5b65b8e8dfa74bc6d6a972f426a6466dc414b
Artyom Narshinov <narshinovartyom@mail.ru>
2024-05-16 11:11:10 +0400

    metrica

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import Head from 'next/head';
 
 import YandexMetrika from 'next-yandex-metrika';
+import Script from 'next/script';
 
 const gilroy = localFont({
   src: [
@@ -59,13 +60,32 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
     <html lang='en'>
       <Head>
         <meta name='yandex-verification' content='c4492d1cc4639f2c' />
-        <YandexMetrika yid={94315700} clickmap={true} trackLinks={true} accurateTrackBounce={true} webvisor={true} />
+        <YandexMetrika yid={97298086} clickmap={true} trackLinks={true} accurateTrackBounce={true} webvisor={true} />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </Head>
       <body>
         <main className={gilroy.className}>
           <ClientProvider>{children}</ClientProvider>
         </main>
+        <Script type='text/javascript'>
+          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(97298086, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });`}
+        </Script>
+        <noscript>
+          <div>
+            <img src='https://mc.yandex.ru/watch/97298086' style={{position: 'absolute', left: '-9999px'}} alt='' />
+          </div>
+        </noscript>
       </body>
     </html>
   );
