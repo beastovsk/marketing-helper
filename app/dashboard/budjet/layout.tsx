67a9c5879bf803267fd8a0d8027333f89ad5cd08
Artyom Narshinov <narshinovartyom@mail.ru>
2024-05-17 16:18:45 +0400

    build fix

--- app/dashboard/budjet/layout.tsx
+++ app/dashboard/budjet/layout.tsx
@@ -1,9 +1,6 @@
-import {Header} from '@/modules/Dashboard/Header/Header';
-import {Sidebar} from '@/modules/Dashboard/Sidebar/Sidebar';
 import '@/src/styles/global.scss';
-import {getCookie} from 'cookies-next';
 import React from 'react';
 
-export default async function RootLayout({data, children}: {data: any; children: React.ReactNode}) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return <>{children}</>;
 }
