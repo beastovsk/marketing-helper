64b2b133b0a8ed17bda7bd991150dc8e2780d955
Artyom Narshinov <narshinovartyom@mail.ru>
2024-06-11 16:05:00 +0400

    d

--- components/UI/PieChart/PieChart.tsx
+++ components/UI/PieChart/PieChart.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import {Pie} from 'react-chartjs-2';
 import {Chart as ChartJS, ArcElement, Tooltip, Legend} from 'chart.js';
+import {Empty} from 'antd';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const PieChart = ({data}) => {
+  if (!data?.length) {
+    return <Empty description='Нет данных' />;
+  }
   // Подготовка данных для графика
   const chartData = {
     labels: data.map((item) => item.label),
     datasets: [
       {
-        data: data.map((item) => item.value),
+        data: data?.length ? data.map((item) => item.value) : [],
         backgroundColor: [
           'rgba(255, 99, 132, 0.7)',
           'rgba(54, 162, 235, 0.7)',
