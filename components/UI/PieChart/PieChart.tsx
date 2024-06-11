db7d8087b9c911a0c860969c918a3c0961f36484
Artyom Narshinov <narshinovartyom@mail.ru>
2024-06-11 22:04:02 +0400

    fix

--- components/UI/PieChart/PieChart.tsx
+++ components/UI/PieChart/PieChart.tsx
@@ -15,8 +15,8 @@ const PieChart = ({data}) => {
       {
         data: data?.length ? data.map((item) => item.value) : [],
         backgroundColor: [
-          'rgba(255, 99, 132, 0.7)',
           'rgba(54, 162, 235, 0.7)',
+          'rgba(255, 99, 132, 0.7)',
           'rgba(255, 206, 86, 0.7)',
           'rgba(75, 192, 192, 0.7)',
           'rgba(153, 102, 255, 0.7)',
