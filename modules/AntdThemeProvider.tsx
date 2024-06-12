3c84e1c0a050abd8a5415a9b99729c0a6b65d619
Artyom Narshinov <narshinovartyom@mail.ru>
2024-06-12 13:03:36 +0400

    d

--- modules/AntdThemeProvider.tsx
+++ modules/AntdThemeProvider.tsx
@@ -45,7 +45,7 @@ function AntdThemeProvider({children}: {children: React.ReactNode}) {
 
     mutate(null, {
       onSuccess: (data) => {
-        if (!data) {
+        if (!data.user) {
           router.push('/auth');
           localStorage.removeItem('email');
           deleteCookie('token');
