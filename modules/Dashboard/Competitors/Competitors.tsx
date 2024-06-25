e9bb9815bb8a4d1fa590e08ece0729c95d2f6363
Artyom Narshinov <narshinovartyom@mail.ru>
2024-06-26 01:14:43 +0400

    d

--- modules/Dashboard/Competitors/Competitors.tsx
+++ modules/Dashboard/Competitors/Competitors.tsx
@@ -21,37 +21,39 @@ const Competitors = () => {
         </PreloadText>
         <div className='mt-5'>
           <div className='box flex-grow'>
-            {statistic?.competitors.map(({label, link, improvement, description, positiveSides, negativeSides}) => (
-              <div className='mb-3'>
-                <PreloadText elementType='h1' className='text-xl'>
-                  {label}{' '}
-                  <Link href={link}>
-                    <LinkOutlined className='text-primary-500 hover:opacity-70 transition-opacity cursor-pointer' />
-                  </Link>
-                </PreloadText>
-                <PreloadText elementType='h3' className='text-lg text-gray-600'>
-                  {description}
-                </PreloadText>
-                <h4 className='text-lg mt-3'>Сильные стороны:</h4>
-                {positiveSides.map((label) => (
-                  <PreloadText elementType='h4' className='text-md'>
-                    <StatusText success={true}>{label}</StatusText>
-                  </PreloadText>
-                ))}
-                <h4 className='text-lg mt-3'>Слабые стороны:</h4>
-                {negativeSides.map((label) => (
-                  <PreloadText elementType='h4' className='text-md'>
-                    <StatusText success={false}>{label}</StatusText>
-                  </PreloadText>
-                ))}
-                <h4 className='text-lg mt-3'>Как можно превзойти?</h4>
-                {improvement.map((label) => (
-                  <PreloadText elementType='h4' className='text-md'>
-                    {label}
-                  </PreloadText>
-                ))}
-              </div>
-            ))}
+            {statistic?.competitors.length
+              ? statistic?.competitors.map(({label, link, improvement, description, positiveSides, negativeSides}) => (
+                  <div className='mb-3'>
+                    <PreloadText elementType='h1' className='text-xl'>
+                      {label}{' '}
+                      <Link href={link}>
+                        <LinkOutlined className='text-primary-500 hover:opacity-70 transition-opacity cursor-pointer' />
+                      </Link>
+                    </PreloadText>
+                    <PreloadText elementType='h3' className='text-lg text-gray-600'>
+                      {description}
+                    </PreloadText>
+                    <h4 className='text-lg mt-3'>Сильные стороны:</h4>
+                    {positiveSides.map((label) => (
+                      <PreloadText elementType='h4' className='text-md'>
+                        <StatusText success={true}>{label}</StatusText>
+                      </PreloadText>
+                    ))}
+                    <h4 className='text-lg mt-3'>Слабые стороны:</h4>
+                    {negativeSides.map((label) => (
+                      <PreloadText elementType='h4' className='text-md'>
+                        <StatusText success={false}>{label}</StatusText>
+                      </PreloadText>
+                    ))}
+                    <h4 className='text-lg mt-3'>Как можно превзойти?</h4>
+                    {improvement.map((label) => (
+                      <PreloadText elementType='h4' className='text-md'>
+                        {label}
+                      </PreloadText>
+                    ))}
+                  </div>
+                ))
+              : null}
           </div>
         </div>
       </div>
